956d570e37d7efb8747acf4ae5e192a61be74016
Anais <a6anaistang@hotmail.co.uk>
2020-07-01 11:48:42 +0100

    created python readme

---
+++ docs/Week3_Python /README.md
@@ -0,0 +1,15 @@
+# Python Training :snake:
+
+**Topics Covered In This Course:**
+- [x] [Introduction](introduction.md)
+- [x] [Variables](variables.py)
+- [x] [Strings](string_casting.py)
+- [x] [List](lists.py)
+- [ ] [Dictionaries](lists.py)
+- [ ] [Tuples](lists.py)
+
+---
+
+> Common Python Interview Questions [HERE](https://www.guru99.com/python-interview-questions-answers.html)
+
+
---
+++ docs/Week3_Python /lists.py
@@ -0,0 +1,16 @@
+# Data Collection
+
+# In python, lists are same as arrays
+# Java arrays in python lists are the same as arrays
+# Why List? - Manage data - access data in order - it gives
+# us the option to add, remove etc.
+
+# Syntax of list: [list] square brackets (tuple) {dictionary - key:value}
+# Tuples are immutable
+
+# Let's create a list
+cities = ["Tokyo", "Paris", "Prague", "Luxembourg", "Buenos Aires"]
+
+# display refers to (print())
+print(cities)
+print(type(cities)) # returns 'List'
